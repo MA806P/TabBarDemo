c8f2f1b7fe9bbd02d593823a1c4f16e3d8421c3d
MA806P <mayp126@126.com>
2016-09-26 11:11:01 +0800

    update

--- TabBarProject/TabBarProject/Temp/FourViewController.m
+++ TabBarProject/TabBarProject/Temp/FourViewController.m
@@ -14,7 +14,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor colorWithRed:arc4random()%255/255.0 green:arc4random()%255/255.0 blue:arc4random()%255/255.0 alpha:1.0];
+    self.view.backgroundColor = [UIColor lightGrayColor];
 }
 
 @end
--- TabBarProject/TabBarProject/Temp/OneViewController.m
+++ TabBarProject/TabBarProject/Temp/OneViewController.m
@@ -13,13 +13,18 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor colorWithRed:arc4random()%255/255.0 green:arc4random()%255/255.0 blue:arc4random()%255/255.0 alpha:1.0];
-    
+//    self.view.backgroundColor = [UIColor colorWithRed:arc4random()%255/255.0 green:arc4random()%255/255.0 blue:arc4random()%255/255.0 alpha:1.0];
+    self.view.backgroundColor = [UIColor grayColor];
     
     UIButton * btn = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 260, 80)];
-    [btn setTitle:@"点击改变红点" forState:UIControlStateNormal];
+    [btn setTitle:@"点击改变图标" forState:UIControlStateNormal];
     [btn addTarget:self action:@selector(change:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn];
+    
+    UIButton * btn2 = [[UIButton alloc] initWithFrame:CGRectMake(100, 200, 260, 80)];
+    [btn2 setTitle:@"点击改变红点" forState:UIControlStateNormal];
+    [btn2 addTarget:self action:@selector(change2:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn2];
     
 }
 
@@ -35,5 +40,16 @@
     vc.tabBarItem.selectedImage = [[UIImage imageNamed:btn.isSelected?@"tabbar_music_selected":@"tabbar_profile_selected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
     
 }
+
+
+- (void)change2:(UIButton *)btn
+{
+    btn.selected = !btn.isSelected;
+    
+    UIViewController * vc =[self.tabBarController.viewControllers objectAtIndex:1];
+    
+    vc.tabBarItem.badgeValue = btn.isSelected ? @"1" : nil;
+}
+
 
 @end
