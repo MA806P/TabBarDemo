2efd5d2ba748df41f927e979712a78c14a7f52bd
MA806P <mayp126@126.com>
2016-09-26 11:08:05 +0800

    simple tabbar modified

---
+++ TabBarProject/TabBarProject/TabBar/BCTabBarController/BCTabBar.h
@@ -0,0 +1,13 @@
+//
+//  BCTabBar.h
+//  TabBarProject
+//
+//  Created by 159CaiMini02 on 16/9/26.
+//  Copyright © 2016年 myz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCTabBar : UITabBar
+
+@end
---
+++ TabBarProject/TabBarProject/TabBar/BCTabBarController/BCTabBar.m
@@ -0,0 +1,21 @@
+//
+//  BCTabBar.m
+//  TabBarProject
+//
+//  Created by 159CaiMini02 on 16/9/26.
+//  Copyright © 2016年 myz. All rights reserved.
+//
+
+#import "BCTabBar.h"
+
+@implementation BCTabBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
